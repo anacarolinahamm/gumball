50857fb7283072ff9891f932d77b07f5ca98cc30
anacarolinahamm <163419286+anacarolinahamm@users.noreply.github.com>
2024-03-15 10:08:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src="https://img.olhardigital.com.br/wp-content/uploads/2021/02/gumball.jpg"
+}
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src="https://www.layerlemonade.com/wp-content/webp-express/webp-images/uploads/2016/06/CN-Capa-LayerLemonade-360x240.png.webp"
+}
